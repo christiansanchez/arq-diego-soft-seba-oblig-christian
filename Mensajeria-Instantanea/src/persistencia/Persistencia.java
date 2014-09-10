fbe2640e6085bbe0b51d5385a361eb14deaef47c
chjse1987@gmail.com <chjse1987@gmail.com@c2c1b54f-1749-1dc7-e6c8-de16cab3448d>
2014-09-10 01:36:40 +0000

    Se agrego persistir ip y mensaje. Aun no se agrego que persista un mensaje a continuacion de otro.

--- Mensajeria-Instantanea/src/persistencia/Persistencia.java
+++ Mensajeria-Instantanea/src/persistencia/Persistencia.java
@@ -1,4 +1,8 @@
-package persistencia;
+package persistencia; 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import javax.management.openmbean.InvalidOpenTypeException;
 
 public class Persistencia 
 {
@@ -8,5 +12,22 @@ public class Persistencia
 		 * 1. agrego al archivo de texto que contiene el historial de mensajes recibidos
 		 *    el mensaje recibido junto con la dirección IP del emisor
 		 */
+		try
+		{ 	
+			FileOutputStream archivo = new FileOutputStream("src/persistencia/historial.txt");
+			ObjectOutputStream objeto = new ObjectOutputStream(archivo);
+			String dataPersistir = ipEmisor + ": " + mensaje; 
+			objeto.writeObject (dataPersistir);
+			objeto.close();
+			archivo.close();
+		}
+		catch(InvalidOpenTypeException e)
+		{
+			e.printStackTrace();			
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
