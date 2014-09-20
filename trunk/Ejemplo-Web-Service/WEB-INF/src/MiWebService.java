04acf67ef8e98e2c085849739a16276373b3faff
diegoarinc@gmail.com <diegoarinc@gmail.com@c2c1b54f-1749-1dc7-e6c8-de16cab3448d>
2014-09-20 17:39:01 +0000

    Source Ejemplo-Web-Service

---
+++ trunk/Ejemplo-Web-Service/WEB-INF/src/MiWebService.java
@@ -0,0 +1,13 @@
+
+public class MiWebService
+{
+	public String saludar (String nombre)
+	{
+		return ("Hola " + nombre);
+	}
+
+	public String despedir (String nombre)
+	{
+		return ("Chau " + nombre);
+	}
+}
