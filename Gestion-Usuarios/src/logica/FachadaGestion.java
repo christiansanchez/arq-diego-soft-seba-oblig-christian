19c5643ba2b9fba51c8ca9a0940298ef3ab16f31
sequeira.sebastian <sequeira.sebastian@c2c1b54f-1749-1dc7-e6c8-de16cab3448d>
2014-09-25 23:22:01 +0000

    Se agregan los métodos crearUsuario, borrarUsuario y listarUsuarios

--- Gestion-Usuarios/src/logica/FachadaGestion.java
+++ Gestion-Usuarios/src/logica/FachadaGestion.java
@@ -1,17 +1,88 @@
 package logica;
 
+import logica.LoginStub.CreateUser;
+import logica.LoginStub.ExistsUser;
+import logica.LoginStub.ExistsUserResponse;
+import logica.LoginStub.ListUsersResponse;
+import logica.LoginStub.RemoveUser;
+
 public class FachadaGestion {
 
 	//Contara con los metodos para ejecutar los requerimientos de gestion de usuarios
 	//(Crear usuario, borrar usuario y listar usuarios).
 	//Esta clase se conecta remotamente con el web service y hara uso de sus servicios.
 	private static FachadaGestion instancia = null;
-		
+
 	public synchronized static FachadaGestion getInstancia ()
 	{
 		if (instancia == null)
 			instancia = new FachadaGestion();
 		return instancia;
 	}
+
+	public void crearUsuario(String name, String pwd){
+		//Controlar que el usuario no exista
+		String url = "http://127.0.0.1:8080/axis2/services/" + "Login.LoginHttpEndpoint/";
+		LoginStub cliente;
+		
+		ExistsUser reqExistsUsr = new ExistsUser();
+		ExistsUserResponse respExistsUsr = new ExistsUserResponse();
+		
+		reqExistsUsr.setName(name);
+		
+		cliente = new LoginStub(url);
+		respExistsUsr = cliente.existsUser(reqExistsUsr);
+		
+		if(!respExistsUsr.get_return()){
+			CreateUser cu = new CreateUser();
+			cu.setName(name);
+			cu.setPwd(pwd);
+			
+			cliente.createUser(cu);
+		}
+		else{
+			//Mensaje el usuario ya exsite
+		}
+		
+		
+		
+	}
 	
+	public void borrarUsuario(String name){
+		//Controlar que el usuario no exista
+		String url = "http://127.0.0.1:8080/axis2/services/" + "Login.LoginHttpEndpoint/";
+		LoginStub cliente;
+		
+		ExistsUser reqExistsUsr = new ExistsUser();
+		ExistsUserResponse respExistsUsr = new ExistsUserResponse();
+		
+		reqExistsUsr.setName(name);
+		
+		cliente = new LoginStub(url);
+		respExistsUsr = cliente.existsUser(reqExistsUsr);
+		
+		if(respExistsUsr.get_return()){
+			RemoveUser remUser = new RemoveUser();
+			remUser.setName(name);
+			
+			cliente.removeUser(remUser);
+		}
+		else{
+			//Mensaje el usuario no exsite
+		}
+		
+	}
+	
+	public String listarUsuario(){
+		String url = "http://127.0.0.1:8080/axis2/services/" + "Login.LoginHttpEndpoint/";
+		LoginStub cliente;
+		
+		ListUsersResponse listUsrResp = new ListUsersResponse();
+		
+		cliente = new LoginStub(url);
+		listUsrResp = cliente.listUsers();
+		
+		return listUsrResp.get_return();
+	}
+
 }
