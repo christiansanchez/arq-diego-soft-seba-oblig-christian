b86bb739c1a1cbf07cf23b05cd59960c1132d4a3
sequeira.sebastian <sequeira.sebastian@c2c1b54f-1749-1dc7-e6c8-de16cab3448d>
2014-09-28 19:43:05 +0000

    Implementación de clases de persistencia. Primera versión

--- trunk/Gestion-Usuarios/src/persistencia/PersistenciaGestion.java
+++ trunk/Gestion-Usuarios/src/persistencia/PersistenciaGestion.java
@@ -1,8 +1,139 @@
 package persistencia;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+
+import javax.management.openmbean.InvalidOpenTypeException;
+
 public class PersistenciaGestion {
 
 	//Almacena los usuarios en un archivo de texto local.
 	
+	public void persistirUsuarios (String name, String pwd)
+	{
+		
+		try
+		{ 	
+			FileOutputStream archivo = new FileOutputStream("src/persistencia/usuarios.txt", true);
+			ObjectOutputStream objeto = new ObjectOutputStream(archivo);
+
+			objeto.writeObject (name + ";" + pwd);
+			objeto.close();
+			archivo.close();
+		}
+		catch(InvalidOpenTypeException e)
+		{
+			e.printStackTrace();
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 	
+	public boolean validarUsuario (String name, String pwd){
+		try {
+			FileInputStream archivo = new FileInputStream("src/persistencia/usuarios.txt");
+			ObjectInputStream objeto = new ObjectInputStream(archivo);
+			String strObj = new String();
+			String nameObj = new String();
+			String pwdObj = new String();
+			String [] arrStrObj = null;
+			
+			List listaObj = (List)objeto.readObject();
+			
+			for(int i=0 ; i<listaObj.size() ; i++){
+				Object obj = listaObj.get(i);
+				strObj = (String)obj;
+				arrStrObj = strObj.split(";");
+				nameObj = arrStrObj[0];
+				pwdObj = arrStrObj[1];
+				
+				if(nameObj.equalsIgnoreCase(name) && pwdObj.equals(pwd))
+					return true;
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public boolean existeUsuario(String name){
+		try {
+			FileInputStream archivo = new FileInputStream("src/persistencia/usuarios.txt");
+			ObjectInputStream objeto = new ObjectInputStream(archivo);
+			String strObj = new String();
+			String nameObj = new String();
+			String pwdObj = new String();
+			String [] arrStrObj = null;
+			
+			List listaObj = (List)objeto.readObject();
+			
+			for(int i=0 ; i<listaObj.size() ; i++){
+				Object obj = listaObj.get(i);
+				strObj = (String)obj;
+				arrStrObj = strObj.split(";");
+				nameObj = arrStrObj[0];
+				pwdObj = arrStrObj[1];
+				
+				if(nameObj.equalsIgnoreCase(name))
+					return true;
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public void borrarUsuario (String name){
+		try {
+			FileInputStream archivo = new FileInputStream("src/persistencia/usuarios.txt");
+			ObjectInputStream objeto = new ObjectInputStream(archivo);
+			String strObj = new String();
+			String nameObj = new String();
+			String pwdObj = new String();
+			String [] arrStrObj = null;
+			
+			List listaObj = (List)objeto.readObject();
+			
+			for(int i=0 ; i<listaObj.size() ; i++){
+				Object obj = listaObj.get(i);
+				strObj = (String)obj;
+				arrStrObj = strObj.split(";");
+				nameObj = arrStrObj[0];
+				pwdObj = arrStrObj[1];
+				
+				if(nameObj.equalsIgnoreCase(name)){
+					listaObj.remove(i);
+					break;
+				}
+			}
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
 }
